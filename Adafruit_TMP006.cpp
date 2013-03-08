193c8d4c789ad3ee1c1a4b8c9fabe69d982dc41b
ladyada <limor@ladyada.net>
2013-03-08 16:00:19 -0500

    Readme! Example!

--- Adafruit_TMP006.cpp
+++ Adafruit_TMP006.cpp
@@ -2,7 +2,7 @@
   This is a library for the TMP006 Temp Sensor
 
   Designed specifically to work with the Adafruit TMP006 Breakout 
-  ----> https://www.adafruit.com/products/391
+  ----> https://www.adafruit.com/products/1296
 
   These displays use I2C to communicate, 2 pins are required to  
   interface
@@ -17,25 +17,40 @@
 #include "Adafruit_TMP006.h"
 #include <util/delay.h>
 
-Adafruit_TMP006::Adafruit_TMP006() {
+//#define TESTDIE 0x0C78
+//#define TESTVOLT 0xFEED
+
+Adafruit_TMP006::Adafruit_TMP006(uint8_t i2caddr) {
+  _addr = i2caddr;
 }
 
 
-boolean Adafruit_TMP006::begin(void) {
+boolean Adafruit_TMP006::begin(uint8_t samplerate) {
   Wire.begin();
 
-  write16(0x02, 0x7500);
+  write16(TMP006_CONFIG, TMP006_CFG_MODEON | TMP006_CFG_DRDYEN | samplerate);
 
   uint16_t mid, did;
   mid = read16(TMP006_MANID);
   did = read16(TMP006_DEVID);
-#if (TMP006_DEBUG == 1)
+#ifdef TMP006_DEBUG
   Serial.print("mid = 0x"); Serial.println(mid, HEX);
   Serial.print("did = 0x"); Serial.println(did, HEX);
 #endif
   if (mid != 0x5449) return false;
   if (did != 0x67) return false;
   return true;
+}
+
+//////////////////////////////////////////////////////
+
+double Adafruit_TMP006::readDieTempC(void) {
+   double Tdie = readRawDieTemperature();
+   Tdie *= 0.03125; // convert to celsius
+#ifdef TMP006_DEBUG
+   Serial.print("Tdie = "); Serial.print(Tdie); Serial.println(" C");
+#endif
+   return Tdie;
 }
 
 double Adafruit_TMP006::readObjTempC(void) {
@@ -45,10 +60,13 @@ double Adafruit_TMP006::readObjTempC(void) {
    Vobj /= 1000; // nV -> uV
    Vobj /= 1000; // uV -> mV
    Vobj /= 1000; // mV -> V
-   Serial.print("Vobj = "); Serial.print(Vobj * 1000000); Serial.println("uV");
    Tdie *= 0.03125; // convert to celsius
-    Serial.print("Tdie = "); Serial.print(Tdie); Serial.println(" C");
    Tdie += 273.15; // convert to kelvin
+
+#ifdef TMP006_DEBUG
+   Serial.print("Vobj = "); Serial.print(Vobj * 1000000); Serial.println("uV");
+   Serial.print("Tdie = "); Serial.print(Tdie); Serial.println(" C");
+#endif
    
    double tdie_tref = Tdie - TMP006_TREF;
    double S = (1 + TMP006_A1*tdie_tref + 
@@ -74,8 +92,10 @@ int16_t Adafruit_TMP006::readRawDieTemperature(void) {
   int16_t raw = read16(TMP006_TAMB);
 
 #if TMP006_DEBUG == 1
-  // MEME
-  //raw = 0xD28;
+
+#ifdef TESTDIE
+  raw = TESTDIE;
+#endif
 
   Serial.print("Raw Tambient: 0x"); Serial.print (raw, HEX);
   
@@ -95,8 +115,9 @@ int16_t Adafruit_TMP006::readRawVoltage(void) {
 
 #if TMP006_DEBUG == 1
 
-  //MEME
-  //raw = 0x91;
+#ifdef TESTVOLT
+  raw = TESTVOLT;
+#endif
 
   Serial.print("Raw voltage: 0x"); Serial.print (raw, HEX);
   float v = raw;
@@ -113,7 +134,7 @@ int16_t Adafruit_TMP006::readRawVoltage(void) {
 uint16_t Adafruit_TMP006::read16(uint8_t a) {
   uint16_t ret;
 
-  Wire.beginTransmission(TMP006_I2CADDR); // start transmission to device 
+  Wire.beginTransmission(_addr); // start transmission to device 
 #if (ARDUINO >= 100)
   Wire.write(a); // sends register address to read from
 #else
@@ -121,8 +142,8 @@ uint16_t Adafruit_TMP006::read16(uint8_t a) {
 #endif
   Wire.endTransmission(); // end transmission
   
-  Wire.beginTransmission(TMP006_I2CADDR); // start transmission to device 
-  Wire.requestFrom(TMP006_I2CADDR, 2);// send data n-bytes read
+  Wire.beginTransmission(_addr); // start transmission to device 
+  Wire.requestFrom(_addr, (uint8_t)2);// send data n-bytes read
 #if (ARDUINO >= 100)
   ret = Wire.read(); // receive DATA
   ret <<= 8;
@@ -138,7 +159,7 @@ uint16_t Adafruit_TMP006::read16(uint8_t a) {
 }
 
 void Adafruit_TMP006::write16(uint8_t a, uint16_t d) {
-  Wire.beginTransmission(TMP006_I2CADDR); // start transmission to device 
+  Wire.beginTransmission(_addr); // start transmission to device 
 #if (ARDUINO >= 100)
   Wire.write(a); // sends register address to read from
   Wire.write(d>>8);  // write data
--- Adafruit_TMP006.h
+++ Adafruit_TMP006.h
@@ -2,7 +2,7 @@
   This is a library for the TMP006 Barometric Pressure & Temp Sensor
 
   Designed specifically to work with the Adafruit TMP006 Breakout 
-  ----> https://www.adafruit.com/products/391
+  ----> https://www.adafruit.com/products/1296
 
   These displays use I2C to communicate, 2 pins are required to  
   interface
@@ -28,9 +28,23 @@
 #else
  #include "WProgram.h"
 #endif
+#include <Adafruit_Sensor.h>
 #include "Wire.h"
 
-#define TMP006_DEBUG 1
+// uncomment for debugging!
+//#define TMP006_DEBUG 1
+
+#define TMP006_CONFIG     0x02
+
+#define TMP006_CFG_RESET    0x8000
+#define TMP006_CFG_MODEON   0x7000
+#define TMP006_CFG_1SAMPLE  0x0000
+#define TMP006_CFG_2SAMPLE  0x0200
+#define TMP006_CFG_4SAMPLE  0x0400
+#define TMP006_CFG_8SAMPLE  0x0600
+#define TMP006_CFG_16SAMPLE 0x0800
+#define TMP006_CFG_DRDYEN   0x0100
+#define TMP006_CFG_DRDY     0x0080
 
 #define TMP006_I2CADDR 0x40
 #define TMP006_MANID 0xFE
@@ -39,17 +53,18 @@
 #define TMP006_VOBJ  0x0
 #define TMP006_TAMB 0x01
 
-
-
-class Adafruit_TMP006 {
+class Adafruit_TMP006  {
  public:
-  Adafruit_TMP006();
-  boolean begin(void);  // by default go highres
+  Adafruit_TMP006(uint8_t addr = TMP006_I2CADDR);
+  boolean begin(uint8_t samplerate = TMP006_CFG_16SAMPLE);  // by default go highres
 
   int16_t readRawDieTemperature(void);
   int16_t readRawVoltage(void);
   double readObjTempC(void);
+  double readDieTempC(void);
+
  private:
+  uint8_t _addr;
   uint16_t read16(uint8_t addr);
   void write16(uint8_t addr, uint16_t data);
 };
