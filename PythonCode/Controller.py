b38aa0a82961ff056d26ec09eabbe6bb8226238c
Gregory S. Kielian <gregory.kielian@gmail.com>
2013-12-22 04:20:38 -0800

    Create Controller.py

---
+++ PythonCode/Controller.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+#USAGE: do sudo python Controller.py NAME_OF_FIREBASE
+#Just use the name of your firebase without "https://" nor the  "firebaseio.com"
+ 
+from firebase import firebase
+import serial, time, sys
+from subprocess import *
+
+
+
+#find out which /dev/ttyACM it is, and return the first result
+serial_port=check_output("find /dev/ -name ttyACM* | head -n 1", shell=True)
+#ser = serial.Serial('/dev/ttyACM', 9600, timeout = 0.1)
+ser = serial.Serial( serial_port.rstrip(), 9600, timeout = 0.1)
+
+def send( theinput ):
+  ser.write( theinput )
+  time.sleep(0.01)
+  
+
+def send_and_receive( theinput ):
+  ser.write( theinput )
+  while True:
+    try:
+      time.sleep(0.01)
+      state = ser.readline()
+      print state
+      break
+    except:
+      pass
+  time.sleep(0.01)
+
+  
+firebase = firebase.FirebaseApplication("https://testbed-firebase.firebaseio.com", None)
+
+while True:
+  try:
+    result = firebase.get('/telerobot/drive', None)
+    if result['direction']=="forward":
+     send('1')
+    elif result['direction']=="backward":
+      send('2')
+    elif result['direction']=="left":
+      send('3')
+    elif result['direction']=="right":
+      send('4')
+    elif result['direction']=="stop":
+      send('5')
+    time.sleep(0.1)
+  except:
+    time.sleep(.1)
+    pass
+
